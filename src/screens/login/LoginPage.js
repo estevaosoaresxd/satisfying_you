451598e1a28ca6a3b57102e880cdb7dc2eb2cbf2
Estevão Soares <estevao.silva@4innovation.com.br>
2023-08-24 11:35:04 -0300

    changes: add js in App

---
+++ src/screens/login/LoginPage.js
@@ -0,0 +1,39 @@
+
+import React from 'react';
+
+import {
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+const LoginPage = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+
+
+export default LoginPage;
