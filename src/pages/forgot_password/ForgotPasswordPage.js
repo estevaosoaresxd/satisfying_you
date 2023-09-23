b8ff219c887cccf6ef6145642f39eb08940cb322
Rafaaaugg <rafaelmendes@alunos.utfpr.edu.br>
2023-09-23 19:56:33 -0300

    criacao das telas de registro e esqueceuasenha

--- src/pages/forgot_password/ForgotPasswordPage.js
+++ src/pages/forgot_password/ForgotPasswordPage.js
@@ -1,13 +1,58 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Container} from '../../components/Container';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Container } from '../../components/Container';
+import { InputValidator } from '../../components/InputValidator';
+import { TextDefault } from '../../components/TextDefault';
+import { ButtonDefault } from '../../components/ButtonDefault';
+import { Column } from '../../components/Column';
+import { validateEmail } from '../../utils/validators/email_validator';
 
-const ForgotPasswordPage = ({navigation, route}) => {
-  return <Container style={styles.container}></Container>;
+const ForgotPasswordPage = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState(false);
+
+  const recupera = () => {
+    setError(false);
+
+    if (validateEmail(email)) {
+      navigation.navigate('login');
+    } else {
+      setError(true);
+    }
+  };
+
+  return <Container style={styles.container}>
+    <Column>
+      <InputValidator text="E-mail" onChange={setEmail} value={email} />
+      <View style={{ height: 10 }}></View>
+      {error ? (
+        <TextDefault style={styles.error}>
+          E-mail parece ser inválido
+        </TextDefault>
+      ) : null}
+      <View style={{ height: 15 }}></View>
+      <ButtonDefault
+        text="RECUPERAR"
+        style={styles.buttonRecuperar}
+        onTap={() => recupera()}
+      />
+    </Column>
+  </Container>;
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    paddingHorizontal: '15%',
+    paddingVertical: '10%'
+  },
+  buttonRecuperar: {
+    backgroundColor: '#37BD6D',
+    paddingVertical: 10,
+    marginVertical: 30,
+  },
+  error: {
+    color: '#FD7979'
+  }
 });
 
-export {ForgotPasswordPage};
+export { ForgotPasswordPage };
--- src/pages/register/RegisterPage.js
+++ src/pages/register/RegisterPage.js
@@ -1,13 +1,87 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Container} from '../../components/Container';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Container } from '../../components/Container';
+import { InputValidator } from '../../components/InputValidator';
+import { TextDefault } from '../../components/TextDefault';
+import { ButtonDefault } from '../../components/ButtonDefault';
+import { Column } from '../../components/Column';
+import { validateEmail } from '../../utils/validators/email_validator';
 
-const RegisterPage = ({navigation, route}) => {
-  return <Container style={styles.container}></Container>;
+const RegisterPage = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordRep, setPasswordRep] = useState('');
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [errorSenha, setErrorSenha] = useState(false);
+
+  const cadastra = () => {
+    setErrorEmail(false);
+    setErrorSenha(false);
+    if (validateEmail(email) === false) {
+      setErrorEmail(true);
+    }
+    if (password !== passwordRep) {
+      setErrorSenha(true);
+    }
+    if (setErrorEmail === true && setErrorSenha === true) {
+      navigation.navigate('login');
+    }
+  };
+
+  return <Container style={styles.container}>
+    <Column>
+      <InputValidator text="E-mail" onChange={setEmail} value={email} />
+      <View style={{ height: 10 }}></View>
+      <InputValidator
+        text="Senha"
+        value={password}
+        onChange={setPassword}
+        isPassword={true}
+      />
+      <View style={{ height: 10 }}></View>
+      <InputValidator
+        text="Repetir Senha"
+        value={passwordRep}
+        onChange={setPasswordRep}
+        isPassword={true}
+      />
+      {errorEmail ? (
+        <TextDefault style={styles.error}>
+          E-mail parece ser inválido
+        </TextDefault>
+      ) : null}
+
+      {errorSenha ? (
+        <TextDefault style={styles.error}>
+          O campo repetir senha difere da senha
+        </TextDefault>
+      ) : null}
+
+      <View style={{ height: 15 }}></View>
+      <ButtonDefault
+        text="Cadastrar"
+        style={styles.buttonCadastrar}
+        onTap={() => cadastra()}
+      />
+    </Column>
+  </Container>
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    paddingHorizontal: '15%',
+    paddingVertical: '1%'
+  },
+  buttonCadastrar: {
+    fontWeight: 50,
+    backgroundColor: '#37BD6D',
+    paddingVertical: 10,
+    marginVertical: 10,
+  },
+  error: {
+    color: '#FD7979'
+  }
+
 });
 
-export {RegisterPage};
+export { RegisterPage };
