ebc00a7d1de8948381b59c65df05626f284f7fa8
Moretao <marcelomoretao@gmail.com>
2023-09-28 19:05:04 -0300

    Novo cadastro

--- src/pages/modify_search/ModifySearchPage.js
+++ src/pages/modify_search/ModifySearchPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Container} from '../../components/Container';
+import { StyleSheet } from 'react-native';
+import { Container } from '../../components/Container';
+import { TextInput, DatePicker, Button } from 'react-native-paper';
 
-const ModifySearchPage = ({navigation, route}) => {
-  const {type} = route.params;
+const ModifySearchPage = ({ navigation, route }) => {
+  const { type } = route.params;
 
   const getTitle = () => {
     switch (type) {
@@ -16,17 +17,48 @@ const ModifySearchPage = ({navigation, route}) => {
     }
   };
 
+  const [nome, setNome] = React.useState('');
+  const [data, setData] = React.useState(new Date());
+
+  const handleNomeChange = (text) => {
+    setNome(text);
+  };
+
+  const handleDataChange = (data) => {
+    setData(data);
+  };
+
+  const handleCadastro = () => {
+    // Fazer algo para cadastrar a pesquisa
+  };
+
   React.useEffect(() => {
     navigation.setOptions({
       title: getTitle(),
     });
   }, [navigation]);
 
-  return <Container style={styles.container}></Container>;
+  return (
+    <Container style={styles.container}>
+      <TextInput
+        label="Nome"
+        value={nome}
+        onChange={handleNomeChange}
+      />
+      <DatePicker
+        value={data}
+        onChange={handleDataChange}
+      />
+      <Button
+        title="Cadastrar"
+        onPress={handleCadastro}
+      />
+    </Container>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {},
 });
 
-export {ModifySearchPage};
+export { ModifySearchPage };
